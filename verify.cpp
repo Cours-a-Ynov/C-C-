d04e9eb95335721d35896d3224f73e60236ad857
Karrwolf <maloloyerviaud@gmail.com>
2021-05-20 09:18:16 +0200

    First

---
+++ verify.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const string punctuation = "!.,";
+
+bool verify(string password){
+    int loverCase = 0;
+    int upperCase = 0;
+    bool containsPunct = false;
+    for( char c : password){
+        if( c >= 'A' && c <= '2'){
+            
+        }
+    }
+}
+
+int main(){
+    string password;
+    cout << "Please enter a password : " <<endl;
+    cin >> password;
+    cout << (verify)
+}
